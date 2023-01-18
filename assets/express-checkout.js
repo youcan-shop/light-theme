91b7b114dbc3b2c7285b48f62e85124f67029312
ybenadda <117163420+ybenadda@users.noreply.github.com>
2023-01-18 12:28:31 +0100

    wip: express checkout section

---
+++ assets/express-checkout.js
@@ -0,0 +1,17 @@
+async function placeOrder(e) {
+    e.preventDefault()
+    const expressCheckoutForm = document.querySelector('#express-checkout-form');
+    const fields = Object.fromEntries(new FormData(expressCheckoutForm));
+    const productVariantId = variants[0].id;
+
+    const response = await youcanjs.checkout.placeExpressCheckoutOrder({ productVariantId, quantity: 1, fields });
+
+    response
+    .onSuccess(() => {
+        alert('success')
+    })
+    .onValidationErr(() => {
+        alert('error')
+    })
+}
+  
